27e1889aba3a3496d77f93dd1f8053d38b5d7ac0
kurtunderscore <2005591+kurtunderscore@users.noreply.github.com>
2017-08-22 16:42:36 -0400

    Create Multiply.rb

---
+++ Multiply.rb
@@ -0,0 +1,15 @@
+def multiplication(x,y)
+	z = y #Keep the original second number
+	while x != 1 do #Keeping track of how many times to add first number
+		y = y+z # Doing one of the additions
+		x = x-1 
+	end
+	return y
+end
+
+puts "What is the first factor"
+num1 = gets.chomp.to_i
+puts "What is the second factor?"
+num2 = gets.chomp.to_i
+result = multiplication(num1,num2)
+puts "Your product will be #{result}"
